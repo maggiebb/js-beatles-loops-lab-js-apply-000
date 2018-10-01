2b00e673ff3e0af1706e9aad0a467dd1746369fc
Margaret Brett-Bowen <maggie.erickson@gmail.com>
2018-10-01 21:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ function theBeatlesPlay(){
 }
 
 function johnLenninFacts(){
-  const
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 }
